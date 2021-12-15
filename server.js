2417aaa294ba3616f19cc950d0022454efe80039
sathish.u <sathish.u@gdn-commerce.com>
2021-12-15 19:10:13 +0700

    test

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.get('/', function(request, response) {
 app.get('/backend', async (req, res) => {
     try {
         const data = await axios.get('https://www.blibli.com/backend/search/products?sort=&page=1&start=0&searchTerm=iphone&intent=true&merchantSearch=true&multiCategory=true&customUrl=&&channelId=mobile-web&showFacet=false&userIdentifier=536652622&isMobileBCA=false&userLatLong=-6.197754600000001%2C106.8242036&userLocationCity=Kota%20Jakarta%20Pusat%27');
-        const name = data.data.data.products[2].name   
-        console.log('success: ', name)
-        res.send(name)
+        // const name = data.data.data.products[2].name   
+        console.log('success: ', data.data.data)
+        res.json(data.data.data)
     }
     catch {
         console.log('failed')
