8a923435b0bf441d27b1f8216127b57cb15a3f8b
sathish.u <sathish.u@gdn-commerce.com>
2021-12-15 19:28:28 +0700

    change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-app.use('/api', createProxyMiddleware({ target: 'https://www.blibli.com', changeOrigin: true }));
+app.use('/backend/search/products', createProxyMiddleware({ target: 'https://www.blibli.com' }));
 
 app.use(express.static(__dirname + '/public'))
 
@@ -12,15 +12,15 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-app.get('/backend', async (req, res) => {
+app.get('/search', async (req, res) => {
     try {
-        const data = await axios.get('https://www.blibli.com/backend/search/products?sort=&page=1&start=0&searchTerm=iphone&intent=true&merchantSearch=true&multiCategory=true&customUrl=&&channelId=mobile-web&showFacet=false&userIdentifier=536652622&isMobileBCA=false&userLatLong=-6.197754600000001%2C106.8242036&userLocationCity=Kota%20Jakarta%20Pusat%27');
+        const data = await axios.get('/backend/search/products?sort=&page=1&start=0&searchTerm=iphone&intent=true&merchantSearch=true&multiCategory=true&customUrl=&&channelId=mobile-web&showFacet=false&userIdentifier=536652622&isMobileBCA=false&userLatLong=-6.197754600000001%2C106.8242036&userLocationCity=Kota%20Jakarta%20Pusat%27');
         // const name = data.data.data.products[2].name   
         console.log('success: ', data.data.data)
         res.json(data.data.data)
     }
     catch(err) {
-        console.log('failed')
+        console.log(`${err.message}`)
         res.send(`${err.message}`)
     }
 })
