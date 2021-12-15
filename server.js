4d93bac863014881b33a11df5d59cdc27bffc30d
sathish.u <sathish.u@gdn-commerce.com>
2021-12-15 18:27:29 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const axios = require('axios');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/api', createProxyMiddleware({ target: 'https://www.blibli.com', changeOrigin: true }));
+
+app.get('/backend', async (req, res) => {
+    try {
+        const data = await axios.get('https://www.blibli.com/backend/search/products?sort=&page=1&start=0&searchTerm=iphone&intent=true&merchantSearch=true&multiCategory=true&customUrl=&&channelId=mobile-web&showFacet=false&userIdentifier=536652622&isMobileBCA=false&userLatLong=-6.197754600000001%2C106.8242036&userLocationCity=Kota%20Jakarta%20Pusat%27');
+        const name = data.data.data.products[2].name   
+        console.log('success: ', name)
+        res.send(name)
+    }
+    catch {
+        console.log('failed')
+        res.send('failed')
+    }
+})
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+app.listen(port, () => {
+    console.log("server created on " + app.get('port'));
+});
