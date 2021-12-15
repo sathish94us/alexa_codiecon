37977a4a3a473e7e2df9a089b83c3c72d1591876
sathish.u <sathish.u@gdn-commerce.com>
2021-12-15 19:02:33 +0700

    modify

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 
 app.use('/api', createProxyMiddleware({ target: 'https://www.blibli.com', changeOrigin: true }));
 
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
 app.get('/backend', async (req, res) => {
     try {
         const data = await axios.get('https://www.blibli.com/backend/search/products?sort=&page=1&start=0&searchTerm=iphone&intent=true&merchantSearch=true&multiCategory=true&customUrl=&&channelId=mobile-web&showFacet=false&userIdentifier=536652622&isMobileBCA=false&userLatLong=-6.197754600000001%2C106.8242036&userLocationCity=Kota%20Jakarta%20Pusat%27');
